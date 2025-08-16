40bb8cbadbaddf0a7a6eaeb4c6a1b2bb7aeb0b8d
cristianchaparroa <cristianchaparroa@gmail.com>
2025-08-16 02:32:19 -0400

    integrate some wallet information

--- src/types/wallet.ts
+++ src/types/wallet.ts
@@ -7,6 +7,12 @@ export interface WalletProvider {
     disconnect?: () => Promise<void>;
 }
 
+export interface Wallet {
+    isConnected: boolean;
+    address: string;
+    chainId: number;
+}
+
 export interface WalletContext {
     provider: WalletProvider;
     isSupported: boolean;
